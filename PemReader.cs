12c0fbb7707f91aaea3b03e6ba374ea72c164862
faroreJP <creator.farore@gmail.com>
2017-07-16 19:20:03 +0900

    Initial commit

---
+++ PemReader.cs
@@ -0,0 +1,188 @@
+//::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+// PEM Reader
+// @Author : Farore
+// @Date   : 2017/07/16
+//::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+class PemReader {
+
+  //::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+  // Field
+  //::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+  private static readonly string    BeginPublicKeyString  = "-----BEGIN PUBLIC KEY-----";
+  private static readonly string    EndPublicKeyString    = "-----END PUBLIC KEY-----";
+  private static readonly string[]  StringSeparators      = new string[] {System.Environment.NewLine};
+
+  //::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+  // Public Usage
+  //::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+  // Decode PEM formatted public key
+  public static string DecodePublicKey (string pemString) {
+    string[] splitString = pemString.Split(StringSeparators, System.StringSplitOptions.None);
+
+    //------------------------------------------------------------
+    // Validate input string
+    if (splitString[0] != BeginPublicKeyString || splitString[splitString.Length - 1] != EndPublicKeyString) {
+      throw new System.ArgumentException("[PemReader] Invalid input string! : " + System.Environment.NewLine + pemString);
+    }
+
+    //------------------------------------------------------------
+    // Join BASE64 string
+    var base64StringBuilder = new System.Text.StringBuilder();
+    for (int i = 1;i < splitString.Length - 1;i++) {
+      base64StringBuilder.Append(splitString[i]);
+    }
+
+    //------------------------------------------------------------
+    // Decode BASE64 string
+    string base64String = base64StringBuilder.ToString();
+    byte[] berBytes    = System.Convert.FromBase64String(base64String);
+
+    //------------------------------------------------------------
+    // Decode ASN.1 binary
+    var bufferBytes = berBytes;
+
+    // Read tag
+    var tagInfo = ReadTag(bufferBytes);
+    bufferBytes = ReadOn(bufferBytes, tagInfo.TagLength);
+
+    // Read length
+    var lengthInfo = ReadLength(bufferBytes);
+    bufferBytes    = ReadOn(bufferBytes, 1); // TODO
+
+    return string.Format("[TAG] {1}{0}", System.Environment.NewLine, tagInfo.ToString());
+  }
+
+
+  //::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+  // Private
+  //::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+  private static byte[] ReadOn (byte[] bytes, int readLength) {
+    byte[] readBytes = new byte[bytes.Length - readLength];
+    System.Buffer.BlockCopy(bytes, 0, readBytes, 0, readLength);
+    return readBytes;
+  }
+
+  private static TagInfo ReadTag (byte[] tag) {
+      byte                  readCode    = 0;
+      TagInfo.eClassType    classType   = TagInfo.eClassType.Universal;
+      TagInfo.eContentType  contentType = TagInfo.eContentType.Primitive;
+      int                   tagLength   = 0;
+      int                   tagNumber   = 0;
+
+      //------------------------------------------------------------
+      // Read class type
+      readCode = (byte)((tag[0] & 0xC0) >> 6);
+      switch (readCode) {
+        case 0x00:  classType = TagInfo.eClassType.Universal;       break;
+        case 0x01:  classType = TagInfo.eClassType.Application;     break;
+        case 0x02:  classType = TagInfo.eClassType.ContentSpecific; break;
+        case 0x03:  classType = TagInfo.eClassType.Private;         break;
+      }
+
+      //------------------------------------------------------------
+      // Read content type
+      contentType = ((tag[0] & 0x20) == 0x00 ? TagInfo.eContentType.Primitive : TagInfo.eContentType.Constructed);
+
+      //------------------------------------------------------------
+      // Read length
+      readCode = (byte)(tag[0] & 0x1f);
+
+      tagLength = 1;
+      if (readCode == 0x1f) {
+        throw new System.NotSupportedException("Any length tag is not supported!");
+      }
+      else {
+        tagNumber = (int)readCode;
+      }
+
+      //------------------------------------------------------------
+      // Create tag info instance
+      return new TagInfo(classType, contentType, tagLength, tagNumber);
+  }
+
+  private static LengthInfo ReadLength (byte[] berBytes) {
+      byte readCode       = berBytes[0];
+      bool isMsbEnabled   = ((readCode & 0x80) != 0x00);
+      byte bit7to1        = (byte)(readCode & 0x7f);
+      bool isLongDefinite = (isMsbEnabled && bit7to1 != 0x00);
+
+      if (isLongDefinite) {
+        throw new System.NotSupportedException("Long definite length is not supported!");
+      }
+      else {
+        return new LengthInfo((int)bit7to1);
+      }
+  }
+
+  //::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+  // Inner Class
+  //::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+  public class TagInfo {
+    //::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+    // Enum
+    //::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+    public enum eClassType {
+      Universal,
+      Application,
+      ContentSpecific,
+      Private,
+    }
+
+    public enum eContentType {
+      Primitive,
+      Constructed,
+    }
+
+    //::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+    // Field / Property
+    //::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+    public eClassType   ClassType     { get; private set; }
+    public eContentType ContentType   { get; private set; }
+    public int          TagLength     { get; private set; }
+    public int          TagNumber     { get; private set; }
+
+    //::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+    // Constructor
+    //::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+    public TagInfo (eClassType classType, eContentType contentType, int tagLength, int tagNumber) {
+      ClassType   = classType;
+      ContentType = contentType;
+      TagLength   = tagLength;
+      TagNumber   = tagNumber;
+    }
+
+    //::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+    // Public Usage
+    //::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+    public override string ToString () {
+      return string.Format("ClassType : {0}, ContentType : {1}, TagLength : {2}, TagNumber : {3}", ClassType, ContentType, TagLength, TagNumber);
+    }
+  }
+
+  public class LengthInfo {
+    //::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+    // Field
+    //::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+    public bool IsInfinite { get; private set;}
+    public int  Length     { get; private set;}
+
+    //::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+    // Constructor
+    //::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+    public LengthInfo (int length) {
+      IsInfinite = (length == 0);
+      Length     = length;
+    }
+  }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace csharp_pem_reader {
+  class MainClass {
+    public static void Main(string[] args) {
+      string pubKey = 
+@"-----BEGIN PUBLIC KEY-----
+MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAu5ZkhqFQLzl2dOCH2aSf
+K6BG/crDnf2d7JCy0rEkkCU7iH1T55wGKAVPjsWYDXrqezWLdwgW7xIQDk8tF9tT
+rdDGe3kmUqjXo/bNOYT8y6eAuibkYDrtVT0Sh6XBF5+pxDOhQolHV14hTmsJKo9E
+0fI12FA44F0ifwNY+BAQmihCW7X6KqaOOB0nA+bxptcmYVjfUVGPuTdebQjJ83b9
+hJ7BRoXYegWRBNaBR9qvXj1JuwpgO/higWYBU/IZJgfqQC06qsqAAymt+X4GBHLI
+wnR5bB9G0Pk5ni3sLjgyIL9kpmLfL6CmmNcNA5P5ImMMweueGuw6PNZ75KZw+TGf
+FQIDAQAB
+-----END PUBLIC KEY-----";
+
+      string result = PemReader.DecodePublicKey(pubKey);
+      Console.WriteLine(result);
+    }
+  }
+}
